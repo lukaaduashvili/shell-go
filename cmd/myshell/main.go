d7dbe6cb9d2ef6aa98842cfebe87616acd57bd25
lukaaduashvili <lukaaduashvili@gmail.com>
2024-06-02 15:30:54 +0400

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,19 +12,31 @@ var commands = map[string]string{
 }
 
 func main() {
+	runCLI()
+}
+
+func runCLI() {
+	for {
+		command := requestInput()
+		executeCommand(command)
+	}
+}
+
+func requestInput() string {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
-		return
+		return ""
 	}
-
 	command = strings.TrimSpace(command)
+	return command
+}
 
+func executeCommand(command string) {
 	_, ok := commands[command]
 
 	if !ok {
 		fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 	}
-
 }
