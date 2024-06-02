bf89a40ba3d8a3f93b7a7452e855a0e20eb773d5
lukaaduashvili <lukaaduashvili@gmail.com>
2024-06-02 15:14:25 +0400

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,7 +10,7 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	fmt.Fprint(os.Stdout, "$")
+	fmt.Fprint(os.Stdout, "$ ")
 
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
